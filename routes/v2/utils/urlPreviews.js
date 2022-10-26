fc1f3e45be79c2f4415d1e2553ddaa71b938ac7d
Duy Hoang Nguyen <duyhoangnguyen2000@gmail.com>
2022-10-26 16:22:27 -0700

    finished a4

--- routes/v2/utils/urlPreviews.js
+++ routes/v2/utils/urlPreviews.js
@@ -1,7 +1,16 @@
 
 import fetch from 'node-fetch';
-
+import * as cheerio from 'cheerio';
 import parser from 'node-html-parser';
+
+const escapeHTML = str => str.replace(/[&<>'"]/g, 
+  tag => ({
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      "'": '&#39;',
+      '"': '&quot;'
+    }[tag]));
 
 async function getURLPreview(url){
   // TODO: Copy from your code for making url previews in A2 to make this 
@@ -21,31 +30,61 @@ async function getURLPreview(url){
         let ogSiteName = og.filter(meta => meta.attributes.property.includes("sitename")).pop();
         let ogTitle = og.filter(meta => meta.attributes.property.includes("title")).pop();
         const ogDesc = og.filter(meta => meta.attributes.property.includes("description")).pop();
-        const anchor = (ogURL ? `<a href="${ogURL.attributes.content}">` : `<a href="${url}">`);
-        const img = (ogIMG ? `<img src="${ogIMG.attributes.content}" style="max-height: 200px; max-width: 270px;">` : ``);
-        const desc = (ogDesc ? `<p>${ogDesc.attributes.content}</p>` : ``);
+        // const anchor = (ogURL ? `<a href="${ogURL.attributes.content}" >` : `<a href="${url}" >`);
+        // let achor = {};
+        const htmlString = `
+        <div style='max-width: 300px; border: solid 1px; padding: 3px; text-align: center;'>
+            <h2 class="siteName"></h2>
+            <a class="siteURL" href="">
+                <p class="siteTitle"><strong></strong></p>
+            </a>
+            <img class="siteIMG" src="" style="max-height: 200px; max-width: 270px;>
+            <p class="siteDesc"></p>
+        </div>`
+        let parsedHTML = cheerio.load(htmlString)
+        if (ogURL) {
+            parsedHTML("a.siteURL").attr('href', escapeHTML(ogURL.attributes.content));
+        } else {
+            parsedHTML("a.siteURL").attr('href', url);
+        }
+        if (ogIMG) {
+            parsedHTML("img.siteIMG").attr('src', escapeHTML(ogIMG.attributes.content));
+        } else {
+            parsedHTML("img.siteIMG").remove();
+        }
+        if (ogDesc) {
+            parsedHTML("p.siteDesc").text(ogDesc.attributes.content)
+        } else {
+            parsedHTML("p.siteDesc").remove();
+        }
+        // const img = (ogIMG ? `<img src="${ogIMG.attributes.content}" style="max-height: 200px; max-width: 270px;">` : ``);
+        // const desc = (ogDesc ? `<p>${ogDesc.attributes.content}</p>` : ``);
         if (!ogTitle) {
             ogTitle = webPageHTML.querySelector('title');
-            ogTitle = `<p><strong>${ogTitle.innerText}</strong></p>`;
             if (!ogTitle) {
                 ogTitle = url;
+            } else {
+                ogTitle = ogTitle.innerText;
             }
         } else {
-            ogTitle = `<p><strong>${ogTitle.attributes.content}</strong></p>`;
+            ogTitle = ogTitle.attributes.content;
         }
+        parsedHTML("p.siteTitle > strong").text(ogTitle)
         if (!ogSiteName) {
             ogSiteName = webPageHTML.querySelector('meta[name="Author"]');
-            if (!ogSiteName) {
-                ogSiteName = undefined;
-            }
+        } 
+        if (!ogSiteName) {
+            parsedHTML("h2.siteName").remove();
+        } else {
+            parsedHTML("h2.siteName").text(ogSiteName.attributes.content)
         }
-        const SiteName = (ogSiteName ? `<h2>${ogSiteName.attributes.content}</h2>` : ``);
+        // const SiteName = (ogSiteName ? `<h2>${ogSiteName.attributes.content}</h2>` : ``);
 
-        const htmlRes = `
-        <div style='max-width: 300px; border: solid 1px; padding: 3px; text-align: center;'>
-            ${anchor}${SiteName}${ogTitle}${img}${desc}</a>
-        </div>`;
-        return htmlRes;
+        // const htmlRes = `
+        // <div style='max-width: 300px; border: solid 1px; padding: 3px; text-align: center;'>
+        //     ${anchor}${SiteName}${ogTitle}</a>
+        // </div>`;
+        return parsedHTML.html();;
   } catch (err) {
         return err.message;
   }
